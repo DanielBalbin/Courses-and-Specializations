0a1401b9d4e22426168b02c049153d95fdacc69f
DanielBalbin <86888200+DanielBalbin@users.noreply.github.com>
2021-07-07 14:31:06 -0300

    Add files via upload

---
+++ MySQL tutorial for beginners/GROUP_BY.sql
@@ -0,0 +1,17 @@
+-- As tabelas utilizadas são copias das fornecidas pelo curso
+-- Exemplo da utilização da função group by, a tabela é agrupada 
+-- pela customer_id para levantar quantas ordens foram feitas.
+-- Os dados são fusionados com a tabela de clientes para exibir a informação destes
+
+USE sql_store;
+
+SELECT *
+FROM(SELECT  
+    customer_id,
+    -- cn.first_name AS name,
+    COUNT(customer_id) AS orders
+	FROM orders_new onw
+	GROUP BY(customer_id)) table1
+JOIN customers_new table2
+	USING(customer_id)
+ORDER BY orders DESC
