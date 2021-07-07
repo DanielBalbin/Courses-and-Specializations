1ff58930bf173420e958f5002ed5e4dbf6dc4d22
DanielBalbin <86888200+DanielBalbin@users.noreply.github.com>
2021-07-07 13:39:25 -0300

    Update JOIN.sql

--- MySQL tutorial for beginners/JOIN.sql
+++ MySQL tutorial for beginners/JOIN.sql
@@ -1,5 +1,8 @@
 USE sql_store;
 
+-- As tabelas utilizadas são copias das tabelas fornecidas pelo curso
+-- Esse query unifica duas tabelas utilizando o campo customer_id
+
 SELECT  
 	onw.order_id,
     cn.first_name AS name,
@@ -7,4 +10,7 @@ SELECT
 FROM orders_new onw
 JOIN customers_new cn
 	ON cn.customer_id = onw.customer_id
+-- ALternativa para o JOIN quando a junção é realizada com um field de mesmo nome
+-- JOIN customers_new USING(customer_id)
+
 ORDER BY order_date
